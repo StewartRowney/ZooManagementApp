f1dac9a1df75675894b2483457477542146d5697
Ayush <pandeyayushraj4@gmail.com>
2023-11-27 13:06:01 +0000

    Written code + tests for FindAll Zoos

--- src/main/java/com/example/ZooManagementApp/services/ZooService.java
+++ src/main/java/com/example/ZooManagementApp/services/ZooService.java
@@ -1,7 +1,17 @@
 package com.example.ZooManagementApp.services;
 
+import com.example.ZooManagementApp.data.ZooRepository;
+import com.example.ZooManagementApp.entities.Zoo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class ZooService {
+public class ZooService implements IZooService{
+    @Autowired
+    ZooRepository repository;
+    public List<Zoo> findAllZoos() {
+        return repository.findAll();
+    }
 }
