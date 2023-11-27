526d018e00bfaab9a7c56a01429d0ef3f8615bae
Thomas Rands <132161715+TomRands@users.noreply.github.com>
2023-11-27 13:12:28 +0000

    refactor of populator to include entity type

--- src/main/java/com/example/ZooManagementApp/data/Populator.java
+++ src/main/java/com/example/ZooManagementApp/data/Populator.java
@@ -1,7 +1,6 @@
 package com.example.ZooManagementApp.data;
 
-import com.example.ZooManagementApp.entities.Animal;
-import com.example.ZooManagementApp.entities.Zoo;
+import com.example.ZooManagementApp.entities.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -25,29 +24,29 @@ public class Populator {
 
     @EventListener(ContextRefreshedEvent.class)
     public void populate(){
-        Zoo chesterZoo = new Zoo("Chester Zoo", "Upton-by-Chester, Cheshire, England", 27000, BigDecimal.valueOf(19.00), LocalDate.of( 1931,  06, 10));
+        Zoo chesterZoo = new Zoo("Chester Zoo", "Upton-by-Chester, Cheshire, England", 27000, BigDecimal.valueOf(19.00), LocalDate.of( 1931,  6, 10));
         zooRepository.save(chesterZoo);
 
-        Zoo edinburghZoo = new Zoo("Edinburgh Zoo", "Corstorphine,  Edinburgh, Scotland", 2500, BigDecimal.valueOf(22.50), LocalDate.of( 1913,  07, 22));
+        Zoo edinburghZoo = new Zoo("Edinburgh Zoo", "Corstorphine,  Edinburgh, Scotland", 2500, BigDecimal.valueOf(22.50), LocalDate.of( 1913,  7, 22));
         zooRepository.save(edinburghZoo);
 
-        Zoo londonZoo = new Zoo("London Zoo", "Regent's Park, London, England", 20166, BigDecimal.valueOf(27.00), LocalDate.of( 1828,  04, 27));
+        Zoo londonZoo = new Zoo("London Zoo", "Regent's Park, London, England", 20166, BigDecimal.valueOf(27.00), LocalDate.of( 1828,  4, 27));
         zooRepository.save(londonZoo);
 
 
-        Animal tiger = new Animal(chesterZoo, "Barry", "Tiger", LocalDate.of(2013, 3, 11), "Jungle", "angry and ferocious", "meat", "He is playful when calm but extremely aggressive when provoked");
+        Animal tiger = new Mammal(chesterZoo, "Barry", "Tiger", LocalDate.of(2013, 3, 11), "Jungle", "angry and ferocious", "meat", "He is playful when calm but extremely aggressive when provoked", true, false, false);
         animalRepository.save(tiger);
 
-        Animal pigeon = new Animal(chesterZoo, "Henry", "Pigeon", LocalDate.of(2022, 1, 18), "Woods", "calm and chirpy", "seeds", "Annoying and noisy!");
+        Animal pigeon = new Birds(chesterZoo, "Henry", "Pigeon", LocalDate.of(2022, 1, 18), "Woods", "calm and chirpy", "seeds", "Annoying and noisy!", true, false, false);
         animalRepository.save(pigeon);
 
-        Animal snake = new Animal(chesterZoo, "Slimy", "Snake", LocalDate.of(2000, 11, 22), "Rainforest", "Hungry and territorial", "rodents", "Chills most of the time, sleeps and stares alot");
+        Animal snake = new Reptile(chesterZoo, "Slimy", "Snake", LocalDate.of(2000, 11, 22), "Rainforest", "Hungry and territorial", "rodents", "Chills most of the time, sleeps and stares alot", false, true, false);
         animalRepository.save(snake);
 
-        Animal badger = new Animal(edinburghZoo, "Bill", "Badger", LocalDate.of(1996, 3, 9), "Woods", "Likes digging", "rodents, vermin, insects", "Eats whatever it can get its paws on");
+        Animal badger = new Mammal(edinburghZoo, "Bill", "Badger", LocalDate.of(1996, 3, 9), "Woods", "Likes digging", "rodents, vermin, insects", "Eats whatever it can get its paws on", true, false, false);
         animalRepository.save(badger);
 
-        Animal goldfish = new Animal(edinburghZoo, "Goldie", "Goldfish", LocalDate.of(2023, 07, 29), "Ocean", "Literally unknown, does nothing, exists", "flakes", "A very boring/uncool animal");
+        Animal goldfish = new Fish(edinburghZoo, "Goldie", "Goldfish", LocalDate.of(2023, 07, 29), "Ocean", "Literally unknown, does nothing, exists", "flakes", "A very boring/uncool animal", false, false);
         animalRepository.save(goldfish);
     }
 
