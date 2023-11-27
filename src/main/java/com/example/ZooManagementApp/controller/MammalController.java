159c6c8e17c4d8ef48707659418d4a376ac94da8
Thomas Rands <132161715+TomRands@users.noreply.github.com>
2023-11-27 13:19:29 +0000

    Initial commit of mammal controller

---
+++ src/main/java/com/example/ZooManagementApp/controller/MammalController.java
@@ -0,0 +1,14 @@
+package com.example.ZooManagementApp.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SuppressWarnings("unused")
+@Tag(name = "Mammal Api")
+@RequestMapping("/mammals")
+public class MammalController {
+
+
+}
--- src/main/java/com/example/ZooManagementApp/data/IAnimalRepository.java
+++ src/main/java/com/example/ZooManagementApp/data/IAnimalRepository.java
@@ -1,11 +1,17 @@
 package com.example.ZooManagementApp.data;
 
 import com.example.ZooManagementApp.entities.Animal;
+import com.example.ZooManagementApp.entities.Insect;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface IAnimalRepository extends ListCrudRepository<Animal, UUID> {
+
+    @Query(value = "SELECT * FROM Animal WHERE DType = 'Mammal'", nativeQuery = true)
+    List<Insect> findAllMammals();
 }
