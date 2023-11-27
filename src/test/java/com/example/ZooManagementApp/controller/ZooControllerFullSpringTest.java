a1af74d14c91275eabd8873601fdec174b7c64e9
StewartRowney <110835640+StewartRowney@users.noreply.github.com>
2023-11-27 13:28:47 +0000

    Added profile to zoo tests

--- src/test/java/com/example/ZooManagementApp/controller/ZooControllerFullSpringTest.java
+++ src/test/java/com/example/ZooManagementApp/controller/ZooControllerFullSpringTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -12,6 +13,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 @WebMvcTest(ZooController.class)
+@ActiveProfiles("test")
 public class ZooControllerFullSpringTest {
 
     @MockBean
--- src/test/java/com/example/ZooManagementApp/services/ZooServiceFullSpringTest.java
+++ src/test/java/com/example/ZooManagementApp/services/ZooServiceFullSpringTest.java
@@ -5,9 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ZooServiceFullSpringTest {
 
     @MockBean
