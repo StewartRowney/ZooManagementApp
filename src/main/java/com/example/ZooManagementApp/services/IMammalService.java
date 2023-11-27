c9af957560cef9f73c8c5de42d9d31f4d1683a3a
Thomas Rands <132161715+TomRands@users.noreply.github.com>
2023-11-27 13:23:19 +0000

    Initial commit of mammal service and interface

---
+++ src/main/java/com/example/ZooManagementApp/services/IMammalService.java
@@ -0,0 +1,9 @@
+package com.example.ZooManagementApp.services;
+
+import com.example.ZooManagementApp.entities.Mammal;
+
+import java.util.List;
+
+public interface IMammalService {
+    List<Mammal> getAllAMammals();
+}
---
+++ src/main/java/com/example/ZooManagementApp/services/MammalService.java
@@ -0,0 +1,12 @@
+package com.example.ZooManagementApp.services;
+
+import com.example.ZooManagementApp.entities.Mammal;
+
+import java.util.List;
+
+public class MammalService implements IMammalService{
+    @Override
+    public List<Mammal> getAllAMammals() {
+        return null;
+    }
+}
