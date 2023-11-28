aa8b0c6810f72ff872bc9a15c04912d0c58db2f4
abhi1 <abhi1904kale@gmail.com>
2023-11-28 13:34:24 +0000

    Worked on Fish CRUD

--- src/test/java/com/example/ZooManagementApp/services/FishServiceFullSpringTest.java
+++ src/test/java/com/example/ZooManagementApp/services/FishServiceFullSpringTest.java
@@ -3,6 +3,9 @@ package com.example.ZooManagementApp.services;
 import com.example.ZooManagementApp.data.IAnimalRepository;
 import com.example.ZooManagementApp.entities.Animal;
 import com.example.ZooManagementApp.entities.Fish;
+import com.example.ZooManagementApp.entities.Zoo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -32,35 +35,47 @@ public class FishServiceFullSpringTest {
 
     @Test
     void verifyIfRepositoryInvokesFindById() {
-        UUID id= UUID.randomUUID();
+        UUID id = UUID.randomUUID();
         when(mockAnimalRepository.findFishById(id)).thenReturn(Optional.of(new Fish()));
         uut.findFishById(id);
-        verify(mockAnimalRepository,times(1)).findFishById(id);
+        verify(mockAnimalRepository, times(1)).findFishById(id);
     }
 
     @Test
-    void test_UpdateFish_ValidRequest(){
+    void test_UpdateFish_ValidRequest() {
         Fish fish = createAFish();
-        UUID id = fish.getId();
         when(mockAnimalRepository.existsById(any())).thenReturn(true);
         uut.updateFishWithPut(fish);
         verify(mockAnimalRepository, times(1)).save(fish);
     }
 
-    private Fish createAFish() {
-        return null;
-    }
-
     @Test
-    void test_UpdateFish_InvalidRequest_NoId(){
+    void test_UpdateFish_InvalidRequest_NoId() {
         Fish fish = new Fish();
-        assertThrows(ResponseStatusException.class,() -> uut.updateFishWithPut(null));
+        assertThrows(ResponseStatusException.class, () -> uut.updateFishWithPut(null));
     }
 
     @Test
-    void test_UpdateFish_InvalidRequest_IdNotInDatabase(){
-        Fish fish = createAFish();
+    void test_UpdateFish_InvalidRequest_IdNotInDatabase() {
+        Fish fish = new Fish();
         when(mockAnimalRepository.existsById(any())).thenReturn(false);
-        assertThrows(ResponseStatusException.class,() -> uut.updateFishWithPut(fish));
+        assertThrows(ResponseStatusException.class, () -> uut.updateFishWithPut(fish));
+    }
+
+    private Fish createAFish() {
+        ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
+        String json = "{\n" +
+                "    \"id\": \"40ea5519-fcef-4272-b742-e01790ca04c3\",\n" +
+                "    \"name\": \"Chester Zoo\",\n" +
+                "    \"location\": \"Upton-by-Chester, Cheshire, England\",\n" +
+                "    \"capacity\": 27000,\n" +
+                "    \"price\": 19,\n" +
+                "    \"dateOpened\": \"10-06-1931\"\n" +
+                "  }";
+        try {
+            return mapper.readValue(json, Fish.class);
+        } catch (Exception e) {
+            return new Fish();
+        }
     }
 }
