d2473909a63e2378c54d4991c0f2d1e11b669ec2
StewartRowney <110835640+StewartRowney@users.noreply.github.com>
2023-11-28 14:52:07 +0000

    Added getInsectById integration test

--- src/test/java/com/example/ZooManagementApp/integratedTests/InsectIntegratedTest.java
+++ src/test/java/com/example/ZooManagementApp/integratedTests/InsectIntegratedTest.java
@@ -1,6 +1,7 @@
 package com.example.ZooManagementApp.integratedTests;
 
 import com.example.ZooManagementApp.entities.Insect;
+import com.example.ZooManagementApp.entities.Mammal;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -37,6 +41,25 @@ public class InsectIntegratedTest {
     void test_GetAllInsects_ValidRequest() throws Exception {
         Insect[] actualInsects = getAllInsects();
         assertEquals(2, actualInsects.length);
+    }
+
+    @Test
+    void test_GetInsectById_ValidRequest() throws Exception {
+        UUID insectId = UUID.fromString("26ba3474-846b-4a6d-8f43-6df6f981dbe6");
+        MvcResult result =
+                (this.mockMvc.perform(MockMvcRequestBuilders.get("/insects/" + insectId)))
+                        .andExpect(status().isOk())
+                        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                        .andReturn();
+
+        String contentAsJson = result.getResponse().getContentAsString();
+        Insect insect = mapper.readValue(contentAsJson, Insect.class);
+
+        assertAll(
+                () -> assertEquals(insectId, insect.getId()),
+                () -> assertEquals("Terry", insect.getName()),
+                () -> assertEquals("Worm", insect.getSpeciesName())
+        );
     }
 
     private Insect[] getAllInsects() throws Exception {
