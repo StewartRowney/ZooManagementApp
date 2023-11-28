7517f0086435e19a530bde29540ee241607fd6a9
StewartRowney <110835640+StewartRowney@users.noreply.github.com>
2023-11-28 09:49:02 +0000

    Added findMammalById in controller and tested it

--- src/test/java/com/example/ZooManagementApp/controller/MammalControllerFullSpringTest.java
+++ src/test/java/com/example/ZooManagementApp/controller/MammalControllerFullSpringTest.java
@@ -8,6 +8,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.UUID;
+
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -15,15 +18,23 @@ import static org.mockito.Mockito.verify;
 public class MammalControllerFullSpringTest {
 
     @MockBean
-    IMammalService mammalService;
+    IMammalService mockMammalService;
 
     @Autowired
     MockMvc mockMvc;
 
     @Test
-    void testMammalServiceCalledForGetAllMammals() throws Exception {
+    void test_GetAllMammals_ValidRequest() throws Exception {
         MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/mammals");
         mockMvc.perform(requestBuilder);
-        verify(mammalService,times(1)).findAllMammals();
+        verify(mockMammalService,times(1)).findAllMammals();
+    }
+
+    @Test
+    void test_GetMammalById_ValidRequest() throws Exception {
+        UUID mammalId = UUID.randomUUID();
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/mammals/" + mammalId);
+        mockMvc.perform(requestBuilder);
+        verify(mockMammalService, times(1)).findMammalById(mammalId);
     }
 }
