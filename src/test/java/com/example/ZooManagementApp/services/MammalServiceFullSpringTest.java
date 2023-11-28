0ac98db313a23fdbcb5f929f6d24f8023a0aa47f
StewartRowney <110835640+StewartRowney@users.noreply.github.com>
2023-11-28 13:11:58 +0000

    Revert "Added addMammal to service and tested it"
    This reverts commit f980fbd814ae439424ac9d5d97d9f2c2e93be150.

--- src/test/java/com/example/ZooManagementApp/services/MammalServiceFullSpringTest.java
+++ src/test/java/com/example/ZooManagementApp/services/MammalServiceFullSpringTest.java
@@ -1,13 +1,7 @@
 package com.example.ZooManagementApp.services;
 
 import com.example.ZooManagementApp.data.IAnimalRepository;
-import com.example.ZooManagementApp.data.ZooRepository;
 import com.example.ZooManagementApp.entities.Mammal;
-import com.example.ZooManagementApp.entities.Zoo;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,13 +20,8 @@ public class MammalServiceFullSpringTest {
     @MockBean
     IAnimalRepository mockAnimalRepository;
 
-    @MockBean
-    ZooRepository mockZooRepository;
-
     @Autowired
     IMammalService uut;
-
-    private final ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
 
     @Test
     void test_FindAllMammals_ValidRequest() {
@@ -57,75 +46,5 @@ public class MammalServiceFullSpringTest {
     @Test
     void test_FindMammalById_InvalidRequest_NullID() {
         assertThrows(ResponseStatusException.class, () -> uut.findMammalById(null));
-    }
-
-    @Test
-    void test_AddMammal_ValidRequest() {
-        Mammal mammal = new Mammal();
-        mammal.setZoo(creatZoo());
-        when(mockZooRepository.existsById(any(UUID.class))).thenReturn(true);
-        uut.addMammal(mammal);
-        verify(mockAnimalRepository, times(1)).save(mammal);
-    }
-
-    @Test
-    void test_AddMammal_InvalidRequest_NotInDatabase() {
-        Mammal mammal = new Mammal();
-        mammal.setZoo(creatZoo());
-        when(mockZooRepository.existsById(any(UUID.class))).thenReturn(false);
-        assertThrows(ResponseStatusException.class, () -> uut.addMammal(mammal));
-    }
-
-    @Test
-    void test_AddMammal_InvalidRequest_HasId() {
-        Mammal mammal = createMammal();
-        assertThrows(ResponseStatusException.class, () -> uut.addMammal(mammal));
-    }
-
-    @Test
-    void test_AddMammal_InvalidRequest_NullMammal() {
-        assertThrows(ResponseStatusException.class, () -> uut.addMammal(null));
-    }
-
-    private Mammal createMammal() {
-        String json = """
-                {
-                  "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-                  "name": "string",
-                  "speciesName": "string",
-                  "birthDate": "28-11-2021",
-                  "habitat": "string",
-                  "behaviour": "string",
-                  "foodType": "string",
-                  "extraInformation": "string",
-                  "hasFur": true,
-                  "hasFins": true,
-                  "hasHooves": true
-                }""";
-
-        try {
-            Mammal mammal = mapper.readValue(json, Mammal.class);
-            mammal.setZoo(creatZoo());
-            return mammal;
-        } catch (JsonProcessingException e) {
-            return new Mammal();
-        }
-    }
-
-    private Zoo creatZoo() {
-        String json = """
-                 {
-                    "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-                    "name": "string",
-                    "location": "string",
-                    "capacity": 0,
-                    "price": 0,
-                    "dateOpened": "12-05-1999"
-                  }""";
-        try {
-            return mapper.readValue(json, Zoo.class);
-        } catch (JsonProcessingException e) {
-            return new Zoo();
-        }
     }
 }
