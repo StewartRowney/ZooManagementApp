c349c6b528254c0a324b2776fe9a52a0c8da5951
StewartRowney <110835640+StewartRowney@users.noreply.github.com>
2023-11-28 13:12:10 +0000

    Revert "Added findMammalById in service and tested it"
    This reverts commit 522e1a5ea5b3f0d21742de7601aa5c2591a6b695.

--- src/test/java/com/example/ZooManagementApp/services/MammalServiceFullSpringTest.java
+++ src/test/java/com/example/ZooManagementApp/services/MammalServiceFullSpringTest.java
@@ -1,18 +1,13 @@
 package com.example.ZooManagementApp.services;
 
 import com.example.ZooManagementApp.data.IAnimalRepository;
-import com.example.ZooManagementApp.entities.Mammal;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Optional;
-import java.util.UUID;
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest
 public class MammalServiceFullSpringTest {
@@ -24,27 +19,8 @@ public class MammalServiceFullSpringTest {
     IMammalService uut;
 
     @Test
-    void test_FindAllMammals_ValidRequest() {
+    void testVerifyRepositoryInvokesGetAllMammals() {
         uut.findAllMammals();
         verify(mockAnimalRepository, times(1)).findAllMammals();
-    }
-
-    @Test
-    void test_FindMammalById_ValidRequest() {
-        UUID mammalId = UUID.randomUUID();
-        when(mockAnimalRepository.findMammalById(mammalId)).thenReturn(Optional.of(new Mammal()));
-        uut.findMammalById(mammalId);
-        verify(mockAnimalRepository, times(1)).findMammalById(mammalId);
-    }
-
-    @Test
-    void test_FindMammalById_InvalidRequest_NotInDatabase() {
-        UUID mammalId = UUID.randomUUID();
-        assertThrows(ResponseStatusException.class, () -> uut.findMammalById(mammalId));
-    }
-
-    @Test
-    void test_FindMammalById_InvalidRequest_NullID() {
-        assertThrows(ResponseStatusException.class, () -> uut.findMammalById(null));
     }
 }
