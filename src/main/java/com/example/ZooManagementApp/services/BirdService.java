1c430954a62ec412a64d44528b9a413edd115be9
Ayush <pandeyayushraj4@gmail.com>
2023-11-28 11:14:24 +0000

    Bird Controller Methods added

--- src/main/java/com/example/ZooManagementApp/services/BirdService.java
+++ src/main/java/com/example/ZooManagementApp/services/BirdService.java
@@ -2,9 +2,14 @@ package com.example.ZooManagementApp.services;
 
 import com.example.ZooManagementApp.data.IAnimalRepository;
 import com.example.ZooManagementApp.entities.Bird;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class BirdService implements IBirdService {
@@ -19,5 +24,29 @@ public class BirdService implements IBirdService {
     @Override
     public List<Bird> findAllBirds() {
         return animalRepository.findAllBirds();
+    }
+
+    @Override
+    public Bird findBirdById(UUID id) {
+        if (id == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Bird to update must have an Id");
+        }
+        return animalRepository.findBirdById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Zoo with id: "+ id+ " not found"));
+    }
+
+    @Override
+    public Bird addNewBird(Bird bird) {
+        return null;
+    }
+
+    @Override
+    public Bird updateBirdWithPut(Bird bird) {
+        return null;
+    }
+
+    @Override
+    public void removeBirdById(UUID id) {
+
     }
 }
--- src/main/java/com/example/ZooManagementApp/services/IBirdService.java
+++ src/main/java/com/example/ZooManagementApp/services/IBirdService.java
@@ -3,8 +3,19 @@ package com.example.ZooManagementApp.services;
 import com.example.ZooManagementApp.entities.Bird;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public interface IBirdService {
     List<Bird> findAllBirds();
+
+    Bird findBirdById(UUID id);
+
+    Bird findBirdByName(String name);
+
+    Bird addNewBird(Bird bird);
+
+    Bird updateBirdWithPut(Bird bird);
+
+    void removeBirdById(UUID id);
 }
