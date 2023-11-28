209e3983511af2b886725aa48e214796a59f06d9
StewartRowney <110835640+StewartRowney@users.noreply.github.com>
2023-11-28 11:29:40 +0000

    Added addMammal to service and tested it

--- src/main/java/com/example/ZooManagementApp/services/MammalService.java
+++ src/main/java/com/example/ZooManagementApp/services/MammalService.java
@@ -1,25 +1,26 @@
 package com.example.ZooManagementApp.services;
 
 import com.example.ZooManagementApp.data.IAnimalRepository;
+import com.example.ZooManagementApp.data.ZooRepository;
 import com.example.ZooManagementApp.entities.Mammal;
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class MammalService implements IMammalService{
 
     private final IAnimalRepository animalRepository;
+    private final ZooRepository zooRepository;
 
     @Autowired
-    public MammalService(IAnimalRepository animalRepository) {
+    public MammalService(IAnimalRepository animalRepository, ZooRepository zooRepository) {
         this.animalRepository = animalRepository;
+        this.zooRepository = zooRepository;
     }
 
     public List<Mammal> findAllMammals() {
@@ -36,6 +37,15 @@ public class MammalService implements IMammalService{
 
     @Override
     public Mammal addMammal(Mammal mammal) {
+        if (mammal == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Mammal to add cannot be null");
+        }
+        else if (mammal.getId() != null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Mammal to add cannot contain an id");
+        }
+        else if (!zooRepository.existsById(mammal.getZoo().getId())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Cannot add Mammal to a zoo that doesn't exist");
+        }
         return animalRepository.save(mammal);
     }
 }
