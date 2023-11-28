cfbc867652c7a10ae8a90b7c45f8bfc3fc39425a
StewartRowney <110835640+StewartRowney@users.noreply.github.com>
2023-11-28 10:53:17 +0000

    Added addMammal to controller and tested it

--- src/test/java/com/example/ZooManagementApp/controller/MammalControllerFullSpringTest.java
+++ src/test/java/com/example/ZooManagementApp/controller/MammalControllerFullSpringTest.java
@@ -1,16 +1,22 @@
 package com.example.ZooManagementApp.controller;
 
+import com.example.ZooManagementApp.entities.Mammal;
 import com.example.ZooManagementApp.services.IMammalService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -22,6 +28,8 @@ public class MammalControllerFullSpringTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    private final ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
 
     @Test
     void test_GetAllMammals_ValidRequest() throws Exception {
@@ -36,5 +44,19 @@ public class MammalControllerFullSpringTest {
         MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/mammals/" + mammalId);
         mockMvc.perform(requestBuilder);
         verify(mockMammalService, times(1)).findMammalById(mammalId);
+    }
+
+    @Test
+    void test_AddMammal_ValidRequest() throws Exception {
+        String json = mapper.writeValueAsString(new Mammal());
+
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.post("/mammals")
+                .contentType(MediaType.APPLICATION_JSON)
+                        .content(json)
+                                .accept(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(requestBuilder).andExpect(MockMvcResultMatchers.status().isCreated());
+
+        verify(mockMammalService, times(1)).addMammal(any(Mammal.class));
     }
 }
