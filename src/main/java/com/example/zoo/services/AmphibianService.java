025fd6aa0fe45a212e6c20931db79f2d9df2daa6
Thomas Rands <132161715+TomRands@users.noreply.github.com>
2023-11-30 14:05:57 +0000

    Webhook test

--- src/main/java/com/example/zoo/services/AmphibianService.java
+++ src/main/java/com/example/zoo/services/AmphibianService.java
@@ -16,6 +16,7 @@ import java.util.UUID;
 public class AmphibianService implements IAmphibianService{
 
     private final IAnimalRepository animalRepository;
+
     private final ZooRepository zooRepository;
 
     @Autowired
