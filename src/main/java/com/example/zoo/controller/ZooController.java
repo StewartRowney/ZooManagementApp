0b3b19987433912f3dbc930ef7956a838f1da417
StewartRowney <110835640+StewartRowney@users.noreply.github.com>
2024-02-02 13:15:50 +0000

    Fixed cross origins

--- src/main/java/com/example/zoo/controller/ZooController.java
+++ src/main/java/com/example/zoo/controller/ZooController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @SuppressWarnings({"unused", "UnusedReturnValue"})
 @Tag(name = "Zoo Api")
